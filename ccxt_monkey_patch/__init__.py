76d55fba18c9cf4a3b3e2ece01d7e802d26891d7
Yuan Yao <Relaed@MacBook-Air.local>
2023-11-10 20:09:26 +1030

    (wip) patch

--- ccxt_monkey_patch/__init__.py
+++ ccxt_monkey_patch/__init__.py
@@ -1,7 +1,30 @@
 from types import ModuleType
+from ccxt.base.types import Entry
 import ccxt
 
+def patch_entry(kf):
+    entry = Entry(
+        'orders/status', 
+        'private', 
+        'POST', 
+        {},
+    )
+    kf.private_post_orders_status = entry
+    kf.privatePostOrdersStatus = entry
+    return kf
+
 def patch(ccxt: ModuleType) -> ModuleType:
-    return ccxt
+    kf = ccxt.krakenfutures()
+
+    # Patch ImplicitAPI entries
+    kf = patch_entry(kf)
+
+    # Make fetchOrder available
+    kf.has['fetchOrder'] = True 
+    
+    # add api
+    kf.api['private']['post'].append('orders/status')
+
+    return kf 
 
 patch(ccxt)
