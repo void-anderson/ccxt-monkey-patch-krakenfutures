bc9a52176f7ee6153177befd3e7f66c952df3d3e
Yuan Yao <Relaed@MacBook-Air.local>
2023-11-12 10:34:14 +1030

    (wip) add customized class

--- ccxt_monkey_patch/__init__.py
+++ ccxt_monkey_patch/__init__.py
@@ -1,5 +1,6 @@
-from ccxt.base.types import Entry
 import ccxt
+from ccxt import krakenfutures as KrakenFuturesCCXT
+from ccxt.base.types import Entry
 
 def patch_entry(kf):
     entry = Entry(
@@ -12,21 +13,21 @@ def patch_entry(kf):
     kf.privatePostOrdersStatus = entry
     return kf
 
-def create_order_status_request()
-    pass
 
-def get_kf_patch():
-    kf = ccxt.krakenfutures()
+def get_kf_patch(krakenfutures):
+    kf = krakenfutures({ 
+        'has': { 'fetchOrder': True } 
+    })
+
+    # add api
+    kf.api['private']['post'].append('orders/status')
 
     # Patch ImplicitAPI entries
     kf = patch_entry(kf)
 
-    # Make fetchOrder available
-    kf.has['fetchOrder'] = True 
-    
-    # add api
-    kf.api['private']['post'].append('orders/status')
-
     return kf 
 
-get_kf_patch(ccxt)
+class KrakenFutures(KrakenFuturesCCXT):
+
+    def create_order_status_request(self, cliOrdIds):
+        pass
